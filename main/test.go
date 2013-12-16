13086e562ad62549760ae2664777757b7a97ac6d
Michael Hendricks <michael@ndrix.org>
2013-12-16 14:48:40 -0700

    Use test methods instead of functions
    Using methods allows us to track some state while running tests.  This
    means we don't have to manually track test numbers.  It also means we're
    structurally similar to Go's testing package so that we can emulate it.
    That should make it easier to port a test from testing to tap.

--- main/test.go
+++ main/test.go
@@ -3,7 +3,8 @@ package main
 import "github.com/mndrix/tap.go"
 
 func main() {
-	tap.Header(2)
-	tap.Ok(1, "first test")
-	tap.Ok(2, "second test")
+	t := tap.New()
+	t.Header(2)
+	t.Ok(true, "first test")
+	t.Ok(true, "second test")
 }
